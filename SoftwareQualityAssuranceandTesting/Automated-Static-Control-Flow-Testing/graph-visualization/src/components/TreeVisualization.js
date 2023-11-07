a4f472f3e2b12eae58be57c544fac6f97f5fc602
rufidatul726 <bsse1226@iit.du.ac.bd>
2023-11-07 14:24:54 +0600

    "ast parsing"

--- SoftwareQualityAssuranceandTesting/Automated-Static-Control-Flow-Testing/graph-visualization/src/components/TreeVisualization.js
+++ SoftwareQualityAssuranceandTesting/Automated-Static-Control-Flow-Testing/graph-visualization/src/components/TreeVisualization.js
@@ -5,16 +5,54 @@ const TreeVisualization = () => {
   const [treeData, setTreeData] = useState(null);
 
   useEffect(() => {
-    // Fetch the JSON data from the file
-    fetch('/example.json') // Use the relative path
+    const convertASTToTreeData = (node) => {
+      const treeData = { 
+        name: node._nodetype ,
+      };
+
+      // console.log(treeData);
+
+      if(node.coord){
+        const coord = node.coord.split(':');
+        const { file, line, column } = { file: coord[0], line: coord[1], column: coord[2] };
+        treeData.attributes = { file, line, column };
+      }
+
+      if(node.body){  
+        treeData.children = convertASTToTreeData(node.body);
+      }
+
+      else if(node.decl){
+        treeData.children = convertASTToTreeData(node.decl);
+      }
+
+      else if(node.block_items){
+        treeData.children = node.block_items.map((child) => convertASTToTreeData(child));
+      }
+      
+      else if (node.ext) {
+        treeData.children = node.ext.map((child) => convertASTToTreeData(child));
+      }
+  
+      return treeData;
+    };
+    // Fetch the JSON data from the file (assuming it's in the public folder)
+    fetch('/astTest.json')
       .then((response) => response.json())
-      .then((data) => setTreeData(data))
+      .then((data) => {
+        const treeData = convertASTToTreeData(data);
+        setTreeData(treeData);
+        console.log(treeData);
+      })
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+  
+
   return (
-    <div style={{ width: '100%', height: '500px', justifyContent: 'center' }}>
+    <div style={{ width: '100%', height: '500px' }}>
       {treeData ? (
+        console.log(treeData),
         <Tree data={treeData} orientation="vertical" />
       ) : (
         <p>Loading data...</p>
